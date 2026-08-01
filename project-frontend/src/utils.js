071d0516228bc0f089c201258a5e471eb2808daf
Thiện Nhân <thiennhan103648@gmail.com>
2026-08-01 23:51:11 +0700

    fix file utils.js

--- project-frontend/src/utils.js
+++ project-frontend/src/utils.js
@@ -1,9 +1,9 @@
-export const getImageUrl = (path) => {
+﻿export const getImageUrl = (path) => {
   if (!path) return '/img/placeholder.jpg';
   if (path.startsWith('http')) return path;
   if (path.startsWith('/storage/')) {
     const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
-    return `${API_URL}${path}`;
+    return ${API_URL};
   }
   return path;
 };
@@ -11,7 +11,8 @@ export const getImageUrl = (path) => {
 export const getAuthToken = () => {
   return localStorage.getItem('token');
 };
+
 export const removeVietnameseTones = (str) => {
   if (!str) return '';
-  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/d/g, 'd').replace(/�/g, 'D');
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D');
 };
