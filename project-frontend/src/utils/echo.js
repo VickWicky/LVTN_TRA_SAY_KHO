d8ba2578be62823e80860b0be9cc05fecd4f0f01
Thiện Nhân <thiennhan103648@gmail.com>
2026-08-02 14:46:26 +0700

    doi reverb sang pusher

--- project-frontend/src/utils/echo.js
+++ project-frontend/src/utils/echo.js
@@ -7,13 +7,10 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 const wsHostUrl = new URL(API_URL);
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY || '4p6tjjbdrdt7trrjp5hc',
-    wsHost: wsHostUrl.hostname,
-    wsPort: wsHostUrl.protocol === 'https:' ? 443 : 6001,
-    wssPort: wsHostUrl.protocol === 'https:' ? 443 : 6001,
-    forceTLS: wsHostUrl.protocol === 'https:',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: '08d57782ce4702ed5f23',
+    cluster: 'ap1',
+    forceTLS: true,
 });
 
 export default echo;
